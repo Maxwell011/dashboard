146f88312fe5b920da1dfbbd9ccb71012c7d98f5
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-22 01:57:17 +0100

    Added components

---
+++ src/components/TopSelling.jsx
@@ -0,0 +1,16 @@
+const TopSelling = () => {
+  return (
+    <>
+      <section>
+        <h1>Top Selling</h1>
+        <p>₦ 0.00</p>
+        <p>Total Sold</p>
+        <div>image</div>
+        <h3>None</h3>
+        <h3>0</h3>
+        <p>Whole units sold</p>
+      </section>
+    </>
+  );
+}
+export default TopSelling
---
+++ src/components/Transactions.jsx
@@ -0,0 +1,10 @@
+const Transactions = () => {
+  return (
+    <>
+      <h2>Sales Overview</h2>
+      <p>Showing overview Jan 2022 - Sep 2022</p>
+      <button>view Transaction</button>
+    </>
+  );
+}
+export default Transactions
