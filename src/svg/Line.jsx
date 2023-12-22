a370d3989478956b49afb96b456ba8831625e8c8
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-22 01:59:04 +0100

    Added icons

---
+++ src/svg/Line.jsx
@@ -0,0 +1,16 @@
+const Line = () => {
+  return (
+    <div>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='857'
+        height='2'
+        viewBox='0 0 857 2'
+        fill='none'
+      >
+        <path d='M0 1L857 1.00007' stroke='#E4E4E4' />
+      </svg>
+    </div>
+  );
+}
+export default Line
