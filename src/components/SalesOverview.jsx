7d46c75e5244984acddab9f971da4fa5d904c743
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-25 23:35:28 +0100

    updated content

--- src/components/SalesOverview.jsx
+++ src/components/SalesOverview.jsx
@@ -7,33 +7,33 @@ import { Button } from "@chakra-ui/react";
 const SalesOverview = () => {
   return (
     <>
-      <Box w='856px' h='325px' borderRadius='16' border="2px">
-        <Box display='flex'>
-          <Box display='flex' flexDirection='column'>
-            <Text color='#191919' fontSize={20}>
+      <Box w="856px" h="325px" borderRadius="16" border="2px">
+        <Box display="flex">
+          <Box display="flex" flexDirection="column">
+            <Text color="#191919" fontSize={20}>
               Sales Overview
             </Text>
-            <Text color='#606060' fontSize={12}>
+            {/* <Text color='#606060' fontSize={12}>
               Showing overview Jan 2022 - Sep 2022
-            </Text>
+            </Text> */}
           </Box>
 
           <Box
-            size='md'
-            height='46px'
-            width='177px'
-            border='2px'
-            borderColor='#CBCBCB'
+            size="md"
+            height="46px"
+            width="177px"
+            border="2px"
+            borderColor="#CBCBCB"
             borderRadius={12}
-            display='flex'
-            alignItems='center'
-            justifyContent='center'
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
           >
             Button
           </Box>
         </Box>
 
-        <Box display='flex'>
+        <Box display="flex">
           <Text>1 Week</Text>
           <Text>1 Month</Text>
           <Text>1 Year</Text>
@@ -41,19 +41,19 @@ const SalesOverview = () => {
 
         <Line />
 
-        <Box display='flex' gap={3}>
+        <Box display="flex" gap={3}>
           <Box
-            w='189px'
-            h='73px'
-            borderRadius='12'
-            border='1px'
-            borderColor='#E4E4E4'
+            w="189px"
+            h="73px"
+            borderRadius="12"
+            border="1px"
+            borderColor="#E4E4E4"
           >
-            <Text color='#4545FE' fontSize={20} fontWeight='600'>
+            <Text color="#4545FE" fontSize={20} fontWeight="600">
               ₦ 0.00
             </Text>
-            <Box display='flex' alignItems='center'>
-              <Text color='#191919' fontSize={14}>
+            <Box display="flex" alignItems="center">
+              <Text color="#191919" fontSize={14}>
                 Withdrawal
               </Text>
               <UpArrowIcon />
@@ -61,17 +61,17 @@ const SalesOverview = () => {
             </Box>
           </Box>
           <Box
-            w='189px'
-            h='73px'
-            borderRadius='12'
-            border='1px'
-            borderColor='#E4E4E4'
+            w="189px"
+            h="73px"
+            borderRadius="12"
+            border="1px"
+            borderColor="#E4E4E4"
           >
-            <Text color='#12D8A0' fontSize={20} fontWeight='600'>
+            <Text color="#12D8A0" fontSize={20} fontWeight="600">
               ₦ 0.00
             </Text>
-            <Box display='flex' alignItems='center'>
-              <Text color='#191919' fontSize={14}>
+            <Box display="flex" alignItems="center">
+              <Text color="#191919" fontSize={14}>
                 Withdrawal
               </Text>
               <UpArrowIcon />
@@ -80,19 +80,19 @@ const SalesOverview = () => {
           </Box>
         </Box>
 
-        <Box display='flex' gap={3}>
+        <Box display="flex" gap={3}>
           <Box
-            w='189px'
-            h='73px'
-            borderRadius='12'
-            border='1px'
-            borderColor='#E4E4E4'
+            w="189px"
+            h="73px"
+            borderRadius="12"
+            border="1px"
+            borderColor="#E4E4E4"
           >
-            <Text color='#4545FE' fontSize={20} fontWeight='600'>
+            <Text color="#4545FE" fontSize={20} fontWeight="600">
               ₦ 0.00
             </Text>
-            <Box display='flex' alignItems='center'>
-              <Text color='#191919' fontSize={14}>
+            <Box display="flex" alignItems="center">
+              <Text color="#191919" fontSize={14}>
                 Withdrawal
               </Text>
               <UpArrowIcon />
@@ -100,17 +100,17 @@ const SalesOverview = () => {
             </Box>
           </Box>
           <Box
-            w='189px'
-            h='73px'
-            borderRadius='12'
-            border='1px'
-            borderColor='#E4E4E4'
+            w="189px"
+            h="73px"
+            borderRadius="12"
+            border="1px"
+            borderColor="#E4E4E4"
           >
-            <Text color='#FF6A6A' fontSize={20} fontWeight='600'>
+            <Text color="#FF6A6A" fontSize={20} fontWeight="600">
               ₦ 0.00
             </Text>
-            <Box display='flex' alignItems='center'>
-              <Text color='#191919' fontSize={14}>
+            <Box display="flex" alignItems="center">
+              <Text color="#191919" fontSize={14}>
                 Withdrawal
               </Text>
               <UpIcon2 />
