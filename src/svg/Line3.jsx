ecd3d28b232b49766ffbf8fa82e66e6f20c21e62
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-26 20:36:23 +0100

    Add Line3 component

---
+++ src/svg/Line3.jsx
@@ -0,0 +1,16 @@
+const Line3 = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="311"
+        height="2"
+        viewBox="0 0 311 2"
+        fill="none"
+      >
+        <path d="M0 1L311 1" stroke="#E4E4E4" />
+      </svg>
+    </div>
+  );
+};
+export default Line3;
